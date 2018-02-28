adf872c2fb680f915cee83a7cd1967a2e6f1bafb
Katie West <katiewest820@gmail.com>
2018-02-27 16:24:50 -0800

    updated about me wording

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -18,7 +18,7 @@ export default function App(){
       <div className="bio">
         <p>I am a Portland Oregon based developer who is driven and passionate about learning. I enjoy collaborating with others, love the challenge
         of learning a new language or framework and am addicted to solving problems that just don't make sense.
-          I strive to always produce clean and efficient code that others can easily understand.</p>
+          I strive to always produce clean and efficient code that is easy to understand.</p>
         <p>When I'm not coding, I love to hike and camp with my dog Izzy, drink delicious PNW IPA's and lose at trivia at my neighborhood pub.
         I hope you enjoy looking at my projects as much as I have enjoyed building them!</p>
       </div>
